727f8bc02bdaf1fbfeae2479039936c1bb96036f
Brandon Spell <brandon.spell@rackspace.com>
2022-11-16 12:50:53 -0600

    update ssm oneliners

--- scripts/powershell/ssm/ssm_oneliners.ps1
+++ scripts/powershell/ssm/ssm_oneliners.ps1
@@ -1,3 +1,26 @@
+#SSM Version
+& "C:\Program Files\Amazon\SSM\amazon-ssm-agent.exe" -version
+
+# set  DNS to use dhcp
+Get-WmiObject win32_NetworkAdapterConfiguration -filter 'ipenabled="true"' | %{$_.SetDNSServerSearchOrder()}
+
+#ensure no proxy
+netsh winhttp reset proxy;
+netsh winhttp show proxy;
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\AmazonSSMAgent" -Name Environment -Value @("http_proxy=","no_proxy=169.254.169.254", "https_proxy=") -PropertyType
+ MultiString -Force;
+Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\AmazonSSMAgent | fl DisplayName,Version,Environment
+
+#restart agent
+spsv AmazonSSMAgent;sasv AmazonSSMAgent -passthru
+
+
+#get logs
+# Get SSM Logs Logs
+$dateFilter = [datetime]::Now.ToString('yyyy-MM-dd hh');
+$logs = gc "$env:ProgramData\Amazon\SSM\Logs\amazon-ssm-agent.log" | ?{$_ -match $dateFilter };$logs
+
+
 ##############################
 # Manually install SSM Agent #
 ##############################
@@ -10,25 +33,4 @@ Start-Process -FilePath $env:USERPROFILE\Desktop\SSMAgent_latest.exe -ArgumentLi
 #clean up
 rm -Force $env:USERPROFILE\Desktop\SSMAgent_latest.exe
 
-###########################
-# SSM Proxy Configuration #
-###########################
-#resource: https://docs.aws.amazon.com/systems-manager/latest/userguide/sysman-install-ssm-proxy.html#ssm-agent-proxy-services
-# Cnofigure Proxy Config
-$serviceKey = "HKLM:\SYSTEM\CurrentControlSet\Services\AmazonSSMAgent"
-$keyInfo = (Get-Item -Path $serviceKey).GetValue("Environment")
-$proxyVariables = @("http_proxy=hostname:port", "https_proxy=hostname:port", "no_proxy=169.254.169.254")
 
-if ($keyInfo -eq $null) {
-    New-ItemProperty -Path $serviceKey -Name Environment -Value $proxyVariables -PropertyType MultiString -Force
-}
-else {
-    Set-ItemProperty -Path $serviceKey -Name Environment -Value $proxyVariables
-}
-Restart-Service AmazonSSMAgent
-
-#Reset proxy config
-Remove-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\AmazonSSMAgent -Name Environment
-Restart-Service AmazonSSMAgent
-netsh winhttp reset proxy
-netsh winhttp show proxy
