c997da821d169b4cdcbc752ba138b3a7d570414b
Brandon Spell <brandon.spell@rackspace.com>
2022-11-16 23:52:34 -0600

    update oneliners

--- scripts/powershell/ssm/ssm_oneliners.ps1
+++ scripts/powershell/ssm/ssm_oneliners.ps1
@@ -1,4 +1,4 @@
-#SSM Version
+#SSM Version ( does not work on SSM versions less than v3.0 )
 & "C:\Program Files\Amazon\SSM\amazon-ssm-agent.exe" -version
 
 # set  DNS to use dhcp
@@ -7,12 +7,14 @@ Get-WmiObject win32_NetworkAdapterConfiguration -filter 'ipenabled="true"' | %{$
 #ensure no proxy
 netsh winhttp reset proxy;
 netsh winhttp show proxy;
-New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\AmazonSSMAgent" -Name Environment -Value @("http_proxy=","no_proxy=169.254.169.254", "https_proxy=") -PropertyType
- MultiString -Force;
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\AmazonSSMAgent" -Name Environment -Value @("http_proxy=","no_proxy=169.254.169.254", "https_proxy=") -PropertyType MultiString -Force;
 Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\AmazonSSMAgent | fl DisplayName,Version,Environment
 
 #restart agent
 spsv AmazonSSMAgent;sasv AmazonSSMAgent -passthru
+
+#oneliner of all the above
+& "C:\Program Files\Amazon\SSM\amazon-ssm-agent.exe" -version;Get-WmiObject win32_NetworkAdapterConfiguration -filter 'ipenabled="true"' | %{$_.SetDNSServerSearchOrder()};netsh winhttp reset proxy;netsh winhttp show proxy;New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\AmazonSSMAgent" -Name Environment -Value @("http_proxy=","no_proxy=169.254.169.254", "https_proxy=") -PropertyType MultiString -Force;Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\AmazonSSMAgent | fl DisplayName,Version,Environment;spsv AmazonSSMAgent;sasv AmazonSSMAgent -passthru
 
 
 #get logs
@@ -32,5 +34,4 @@ Invoke-WebRequest https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest
 Start-Process -FilePath $env:USERPROFILE\Desktop\SSMAgent_latest.exe -ArgumentList "/S"
 #clean up
 rm -Force $env:USERPROFILE\Desktop\SSMAgent_latest.exe
-
 
